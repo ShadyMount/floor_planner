62034f427ebcdefcb26d8f8ab6d6d6fe5836b345
ShadyMount <shady2142@gmail.com>
2024-02-14 20:53:55 +0300

    fix: vite config base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/floor-planner/",
+  base: "/floor_planner/",
 });
